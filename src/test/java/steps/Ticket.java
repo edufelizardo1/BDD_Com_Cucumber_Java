fa56a1cbd8e89a917627f157ebb1291312ca2ae5
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-11-01 09:09:44 -0300

    Create Ticket.java

---
+++ src/test/java/steps/Ticket.java
@@ -0,0 +1,36 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class Ticket {
+    @Dado("^que o ticket é (A.\\d{3})$")
+    public void queOTicketÉAF(String arg0) {
+    }
+
+    @Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
+    public void queOValorDaPassagemÉR$(int arg0, int arg1) {
+    }
+
+    @Dado("^que o nome do passageiro é \"(.*)\"$")
+    public void queONomeDoPassageiroÉ(String arg0) throws Throwable {
+    }
+
+    @Dado("^que o telefone do passageiro é (\\d+)-(\\d+)$")
+    public void queOTelefoneDoPassageiroÉ(int arg0, int arg1) {
+    }
+
+    @Quando("^criar os steps$")
+    public void criarOsSteps() {
+    }
+
+    @Então("^o teste vai funcionar$")
+    public void oTesteVaiFuncionar() {
+    }
+
+    @Dado("^que o ticket especial é A.(\\d+)$")
+    public void queOTicketEspecialÉAB(int arg0) {
+    }
+}
