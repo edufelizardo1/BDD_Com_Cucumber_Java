6ed911ba19387021d4ac9622a39f0842c0e04ff7
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-11-01 09:38:53 -0300

    Update PrazoRunner.java

--- src/test/java/runners/PrazoRunner.java
+++ src/test/java/runners/PrazoRunner.java
@@ -3,7 +3,10 @@ package runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-
+/**
+ * @author edufelizardo1@gmail.com
+ * @version 1.0.0
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features/prazo.feature"},
